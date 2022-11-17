2d0c67814ab254a7ff2084dec031edd9741d0900
Webster Grimes <cwebsterg@mac.com>
2022-11-17 11:16:34 -0500

    updated card classname; removed redundant css

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,7 +2,7 @@ export const CardList = (props) => {
     return (
         <div className="card-display">
             {props.data.map((card) => (
-                <div className="card-base">
+                <div className="card-frame">
                     <h6>Outer Message</h6>
                     {/* need to flesh out the styles */}
                 </div>
