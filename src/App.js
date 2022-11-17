413cf0fe44e47c91167457c5ac1d37e14c83cdb6
Cameron Poulton <Crp2108@hotmail.com>
2022-11-17 12:03:00 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { useState } from 'react'
 import { Header } from "./components/Header";
 import { NavBar } from "./components/NavBar";
 
 function App() {
+  const [login, setLogin] = usestate(null)
+
   return (
     <div className="App">
       <Header />
