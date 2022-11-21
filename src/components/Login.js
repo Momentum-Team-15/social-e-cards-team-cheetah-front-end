e0f63051e216e8fdf712bc843a5e8c2b84f3d841
Cameron Poulton <Crp2108@hotmail.com>
2022-11-21 12:36:21 -0500

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,22 @@
 export const Login = ({setLogin}) => {
-    return ()
-
-    
+    return (
+        <div>
+            <div className="login-box">
+                <div className="field">
+                    <input className="input" type="email" placeholder="Email"/>
+            </div>
+            <div className="field">
+                    <input className="input" type="password" placeholder="Password" />
+            </div>
+                <div className="field">
+                        <p className="control">
+                            <button className="button" 
+                                onClick={() => {setLogin(true)}}>
+                                Login
+                            </button>
+                        </p>
+                    </div>
+            </div>
+        </div>
+    ) 
 }
